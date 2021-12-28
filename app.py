24ee6ca6b0ec7d163b1865dcb4576b6ad070aaba
Jlferrete <joseluisferrete@gmail.com>
2021-12-28 20:30:47 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import cv2
+
+file = "frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier(file)
+
+webcam = cv2.VideoCapture(0)
+
+while True:
+    (_, im) = webcam.read()
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+
+    for (x, y, w ,h) in faces:
+        cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0 , 0), 2)
+
+    cv2.imshow('OpenCV', im)
+
+    key = cv2.waitkey(10)
+    if key == 27:
+        break
